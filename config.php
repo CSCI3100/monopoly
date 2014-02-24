9399cbe7df94052220a26901eba732f919c80a2c
tinghim <tinghim2003@yahoo.com.hk>
2014-02-24 21:38:18 +0800

    Update config.php
    change the name

--- config.php
+++ config.php
@@ -27,7 +27,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "cc admin building";
-$building->name = "stationMarker";
+$building->name = "ccadMarker";
 $building->x = "22.41439581682375";
 $building->y = "114.20845784246922";
 $building->img = "station.png";
@@ -38,7 +38,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "wong fok yuen";
-$building->name = "stationMarker";
+$building->name = "wfyMarker";
 $building->x = "22.414921484418553";
 $building->y = "114.20784629881382";
 $building->img = "station.png";
@@ -50,7 +50,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "chans bldg";
-$building->name = "stationMarker";
+$building->name = "chanMarker";
 $building->x = "22.415366564624026";
 $building->y = "114.20760624110699";
 $building->img = "station.png";
@@ -59,10 +59,9 @@ $building->rent = 100;
 $building->stopNo = 3;
 array_push($buildingArray, $building);
 
-
 $building = new building();
 $building->rname = "Sino";
-$building->name = "stationMarker";
+$building->name = "sinoMarker";
 $building->x = "22.41561947864573";
 $building->y = "114.20752845704556";
 $building->img = "station.png";
@@ -74,7 +73,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "CC staff canteen";
-$building->name = "stationMarker";
+$building->name = "ccstaffMarker";
 $building->x = "22.41627779680755";
 $building->y = "114.2077524214983";
 $building->img = "station.png";
@@ -85,7 +84,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "new eng-bld";
-$building->name = "stationMarker";
+$building->name = "newengMarker";
 $building->x = "22.417300601321244";
 $building->y = "114.2081319540739";
 $building->img = "station.png";
@@ -97,7 +96,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "edu research";
-$building->name = "stationMarker";
+$building->name = "edureMarker";
 $building->x = "22.417162988061555";
 $building->y = "114.20722402632236";
 $building->img = "station.png";
@@ -109,7 +108,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "HSB";
-$building->name = "stationMarker";
+$building->name = "HSBMarker";
 $building->x = "22.418334556046702";
 $building->y = "114.20744396746159";
 $building->img = "station.png";
@@ -120,7 +119,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "shaw sci";
-$building->name = "stationMarker";
+$building->name = "shawsciMarker";
 $building->x = "22.418633335295212";
 $building->y = "114.20802064239979";
 $building->img = "station.png";
@@ -131,7 +130,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "BMSB";
-$building->name = "stationMarker";
+$building->name = "BMSBMarker";
 $building->x = "22.41893087415704";
 $building->y = "114.20898891985416";
 $building->img = "station.png";
@@ -142,7 +141,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "sir run run";
-$building->name = "stationMarker";
+$building->name = "sirrunMarker";
 $building->x = "22.419840843220154";
 $building->y = "114.20710198581219";
 $building->img = "station.png";
@@ -153,7 +152,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "pi chu";
-$building->name = "stationMarker";
+$building->name = "pichuMarker";
 $building->x = "22.419840843220154";
 $building->y = "114.20629061758518";
 $building->img = "station.png";
@@ -166,7 +165,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "water tower";
-$building->name = "stationMarker";
+$building->name = "natowMarker";
 $building->x = "22.420642949970436";
 $building->y = "114.20820839703083";
 $building->img = "station.png";
@@ -177,7 +176,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "14";
-$building->name = "stationMarker";
+$building->name = "14Marker";
 $building->x = "22.421094210464293";
 $building->y = "114.20756734907627";
 $building->img = "station.png";
@@ -188,7 +187,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "15";
-$building->name = "stationMarker";
+$building->name = "15Marker";
 $building->x = "22.420602038919093";
 $building->y = "114.20604787766933";
 $building->img = "station.png";
@@ -199,7 +198,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "16";
-$building->name = "stationMarker";
+$building->name = "16Marker";
 $building->x = "22.420349133970557";
 $building->y = "114.20504473149776";
 $building->img = "station.png";
@@ -210,7 +209,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "lsk";
-$building->name = "stationMarker";
+$building->name = "lskMarker";
 $building->x = "22.419964816745452";
 $building->y = "114.2031229287386";
 $building->img = "station.png";
@@ -221,7 +220,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "LKK";
-$building->name = "stationMarker";
+$building->name = "LKKMarker";
 $building->x = "22.419958618071814";
 $building->y = "114.20292042195797";
 $building->img = "station.png";
@@ -232,7 +231,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "wys";
-$building->name = "stationMarker";
+$building->name = "wysMarker";
 $building->x = "22.42188639223504";
 $building->y = "114.20258648693562";
 $building->img = "station.png";
@@ -243,7 +242,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "SHAW bldg";
-$building->name = "stationMarker";
+$building->name = "shawMarker";
 $building->x = "22.42207111002635";
 $building->y = "114.20151494443417";
 $building->img = "station.png";
@@ -254,7 +253,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "Shaw hostel";
-$building->name = "stationMarker";
+$building->name = "shostelMarker";
 $building->x = "22.423088913046463";
 $building->y = "114.20101471245289";
 $building->img = "station.png";
@@ -265,7 +264,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "jail";
-$building->name = "stationMarker";
+$building->name = "jailMarker";
 $building->x = "22.42580632377871";
 $building->y = "114.20544371008873";
 $building->img = "station.png";
@@ -276,7 +275,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "staff hostel";
-$building->name = "stationMarker";
+$building->name = "staffhosMarker";
 $building->x = "22.425343921419284";
 $building->y = "114.20776315033436";
 $building->img = "station.png";
@@ -287,7 +286,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "park";
-$building->name = "stationMarker";
+$building->name = "parkMarker";
 $building->x = "22.41998093329562";
 $building->y = "114.21285666525364";
 $building->img = "station.png";
@@ -298,7 +297,7 @@ array_push($buildingArray, $building);
 
 $building = new building();
 $building->rname = "AIT";
-$building->name = "stationMarker";
+$building->name = "AITMarker";
 $building->x = "22.416195972121077";
 $building->y = "114.21140559017658";
 $building->img = "station.png";
